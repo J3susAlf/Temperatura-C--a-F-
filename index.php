731ee926ac85233d597e81968c2fa62eef5ecbab
J3susAlf <aguila4alfr3do@gmail.com>
2024-06-22 18:36:06 -0600

    CONVERSIÓN DE TEMPERATURA EN PHP, HTML Y JS

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CONVERSIÓN</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container text-center mt-5">
+        <h1 class="fst-italic">CONVERSIÓN DE TEMPERATURA </h1>
+    </div>
+    <div class="container p-5 bg-warning">
+        <form action="" method="POST">
+            <div class="input-group shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+                <span class="input-group-text">Celsius C°</span>
+                <input id="inputCelsius" type="number" class="form-control" name="Celsius" placeholder="Valor celsius">
+                <input id="inputFahrenheit" type="number" class="form-control" name="Fahrenheit" placeholder="Valor Fahrenheit">
+                <span class="input-group-text">Fahrenheit F°</span>
+            </div>
+        </form>
+        <div class="container mt-3 d-flex justify-content-center">
+            <h3 class="fw-bolder fst-italic p-1">Resultado:</h3>
+            <input id="resultado" type="text" disabled>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+<script>
+    const inputCelsius = document.getElementById('inputCelsius');
+    const inputFahrenheit = document.getElementById('inputFahrenheit');
+    const resultado = document.getElementById('resultado');
+
+    let isUpdating = false;
+
+    function conversion() {
+        if (isUpdating) return;
+
+        isUpdating = true;
+
+        const celsius = parseFloat(inputCelsius.value);
+        const fahrenheit = parseFloat(inputFahrenheit.value);
+
+        if (!isNaN(celsius) && document.activeElement === inputCelsius) {
+            const resultFahrenheit = (celsius * 9 / 5) + 32;
+            inputFahrenheit.value = resultFahrenheit.toFixed(2);
+        } else if (!isNaN(fahrenheit) && document.activeElement === inputFahrenheit) {
+            const resultCelsius = (fahrenheit - 32) * 5 / 9;
+            inputCelsius.value = resultCelsius.toFixed(2);
+        }
+
+        if (inputCelsius.value === '' && inputFahrenheit === '') {
+            resultado.value = '';
+        } else {
+            resultado.value = `${inputCelsius.value} °C = ${inputFahrenheit.value} °F`;
+        }
+        isUpdating = false;
+    }
+
+    inputCelsius.addEventListener('input', conversion);
+    inputFahrenheit.addEventListener('input', conversion);
+</script>
+
+</html>
